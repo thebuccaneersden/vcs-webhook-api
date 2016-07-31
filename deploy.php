927bce6929facf8b695119700d9da29c7d6f3e8b
Hans-Christian Andersen <hans.andersen@phulse.com>
2016-07-31 15:21:00 -0700

    Testing deployment

--- deploy.php
+++ deploy.php
@@ -1,10 +1,15 @@
 <?php
+namespace Deployer;
+
+require __DIR__ . '/vendor/autoload.php';
+use Symfony\Component\Yaml\Yaml;
+
 date_default_timezone_set('UTC');
+
 require 'recipe/common.php';
 require 'recipe/composer.php';
-require 'config/deploy.php';
 
-$config = new Deploy_Config();
+$config = get_configuration_from('config/deployer/deploy.yml');
 
 server( 'droplet1', $config->deploy_host, 22 )
   ->user( $config->user )
@@ -28,11 +33,11 @@ task( 'reload:nginx', function () {
 });
 
 task( 'deploy:start', function() {
-  writeln("<info>Deploying to</info> <fg=cyan>http://full.im.phulse.com</fg=cyan>");
+  writeln("<info>Deploying to</info> <fg=cyan>http://standup.api.public.phulse.com</fg=cyan>");
 });
 
 task( 'deploy:finish', function() {
-  writeln("<info>Finished deployment to</info> <fg=cyan>http://full.im.phulse.com</fg=cyan>");
+  writeln("<info>Finished deployment to</info> <fg=cyan>http://standup.api.public.phulse.com</fg=cyan>");
 });
 
 task('deploy', [
@@ -49,3 +54,21 @@ task('deploy', [
 ])->desc('Deploy your project');
 
 after('deploy', 'deploy:finish');
+
+function get_configuration_from( $file )
+{
+  if( !file_exists( $file ) )
+  {
+    echo "ERROR: Configuration file does not exist or this script does not have permissions to read it\n";
+    exit(2);
+  }
+  try
+  {
+    $config = Yaml::parse( file_get_contents( $file ), false, false, true );
+  } catch( Exception $e ) {
+    echo "ERROR: This file does not contain valid YAML! Please fix it...\n";
+    exit(3);
+  }
+
+  return $config;
+}
